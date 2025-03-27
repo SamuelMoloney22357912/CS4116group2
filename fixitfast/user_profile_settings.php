12d10d44b5ee5832f33bc0822b9557ec7879869d
MaxwellClein22364862 <152290030+MaxwellClein22364862@users.noreply.github.com>
2025-03-27 21:50:42 +0000

    Update user_profile_settings.php

--- fixitfast/user_profile_settings.php
+++ fixitfast/user_profile_settings.php
@@ -5,33 +5,55 @@ include("functions.php");
 
 $user_data = check_login($con);
 
-$status = isset($_GET['status']) ? $_GET['status'] : '';
+$updateMessage = "";
 
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $fName = trim($_POST['first_name']);
+    $lName = trim($_POST['last_name']);
+    $county = trim($_POST['county']);
+    $password = trim($_POST['password']);
+    $confirm_password = trim($_POST['confirm_password']);
 
-if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $fName = $_POST['first_name'];
-    $lName = $_POST['last_name'];
-    $county = $_POST['county'];
-    $password = $_POST['password'];
+    if (!empty($fName) && !empty($lName) && !empty($county)) {
+        try {
+            $stmt = $con->prepare("UPDATE users SET first_name=?, last_name=?, county=? WHERE user_id=?");
+            $stmt->bind_param("sssi", $fName, $lName, $county, $user_data['user_id']);
+            $stmt->execute();
+            $stmt->close();
 
-    if(!empty($fName) && !empty($lName)){
+            if (!empty($password)) {
+                if ($password === $confirm_password) {
+                    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        try{
-            $query = "UPDATE users SET first_name='$fName', last_name='$lName', county='$county'  WHERE user_id='{$user_data['user_id']}'";
-            mysqli_query($con, $query);
+                    $stmt = $con->prepare("UPDATE users SET password=? WHERE user_id=?");
+                    $stmt->bind_param("si", $hashedPassword, $user_data['user_id']);
+                    $stmt->execute();
+                    $stmt->close();
 
-            $Update = "Updated";
-        }catch(mysqli_sql_exception $e){
-            die("Database insert error: " . $e->getMessage());
+                } else {
+                    $updateMessage = "Passwords do not match.";
+                }
+            }
+
+            $stmt = $con->prepare("SELECT * FROM users WHERE user_id=?");
+            $stmt->bind_param("i", $user_data['user_id']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $user_data = $result->fetch_assoc();
+            $stmt->close();
+
+            $_SESSION['user_data'] = $user_data;
+
+            if (empty($updateMessage)) {
+                $updateMessage = "Profile updated successfully!";
+            }
+        } catch (mysqli_sql_exception $e) {
+            die("Database error: " . $e->getMessage());
+        }
+    } else {
+        $updateMessage = "Please fill out all fields.";
     }
-
-    }else{
-        echo("Plese fill out all info");
-
-    }
-
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -41,12 +63,16 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Profile Settings</title>
-    <link rel="stylesheet" href="css/profile_settings.css">
+    <link rel="stylesheet" href="css/user_profile_settings.css">
 </head>
 <body>
 
     <div class="container">
         <div class="sidebar">
+            <div class="input-group">
+                <a href="index.php" class="button back-button">Back to Menu</a>
+            </div>
+
             <ul>
                 <li class="active"><a href="user_profile_settings.php">Profile</a></li>
                 <li><a href="user_listings.php">Listings</a></li>
@@ -56,46 +82,65 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
         <div class="profile-section">
             <h1>User Profile</h1>
-            
-            <?php
-            if ($status == 'success') {
-                echo '<p class="success-message">Profile updated successfully!</p>';
-            }
-            ?>
-            
+
+            <?php if (!empty($updateMessage)) { ?>
+                <p class="success-message"><?php echo htmlspecialchars($updateMessage); ?></p>
+            <?php } ?>
+
             <div class="profile-edit">
                 <div class="profile-image">
-                    <img src="images/<?php echo $user_data['profile_picture']; ?>" alt="Profile Picture">
+                    <img src="images/<?php echo htmlspecialchars($user_data['profile_picture']); ?>" alt="Profile Picture">
                     <a href="edit_picture.php">Edit Profile Picture</a>
                 </div>
 
                 <form method="post">
                     <div class="input-group">
                         <label for="first_name">Edit First Name:</label>
-                        <input type="text" id="first_name" name="first_name" value="<?php echo $user_data['first_name']; ?>">
+                        <input type="text" id="first_name" name="first_name" value="<?php echo htmlspecialchars($user_data['first_name']); ?>">
                     </div>
+
                     <div class="input-group">
                         <label for="last_name">Edit Surname:</label>
-                        <input type="text" id="last_name" name="last_name" value="<?php echo $user_data['last_name']; ?>">
+                        <input type="text" id="last_name" name="last_name" value="<?php echo htmlspecialchars($user_data['last_name']); ?>">
                     </div>
-                    <div class="input-group">
-                        <label for="county">Edit County:</label>
-                        <input type="text" id="county" name="county" value="<?php echo $user_data['county']; ?>">
+
+                    <div>
+                        <label for="county">County:</label><br>
+                        <select class="dropdown" id="county" name="county" required>
+                            <option value="" disabled>Select a County</option>
+                            <?php
+                            $counties = [
+                                "Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Derry", "Donegal", 
+                                "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare", "Kilkenny", 
+                                "Laois", "Leitrim", "Limerick", "Longford", "Louth", "Mayo", "Meath", "Monaghan", 
+                                "Offaly", "Roscommon", "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", 
+                                "Wexford", "Wicklow"
+                            ];
+
+                            foreach ($counties as $countyOption) {
+                                // Check if the county is the one the user has currently selected
+                                $selected = ($countyOption == $user_data['county']) ? 'selected' : '';
+                                echo "<option value='$countyOption' $selected>$countyOption</option>";
+                            }
+                            ?>
+                        </select>
                     </div>
+
                     <div class="input-group">
                         <label for="password">Edit Password:</label>
-                        <input type="password" id="password" name="password">
+                        <input type="password" id="password" name="password" placeholder="*****">
                     </div>
+
                     <div class="input-group">
                         <label for="confirm_password">Confirm Password:</label>
-                        <input type="password" id="confirm_password" name="confirm_password">
+                        <input type="password" id="confirm_password" name="confirm_password" placeholder="*****">
                     </div>
-                    <div class="save-btn">
-                        <input type="submit" value="Save Changes">
-                    </div>
-                </form>
 
-                    <p><?php echo($Update); ?></p>
+                    <div class="input-group">
+                        <input type="submit" value="Save Changes" class="button">
+                    </div>
+                    
+                </form>
             </div>
         </div>
     </div>
